f4536561aabae6cfd3f503c57fb64a2e22177ce8
Pipe104 <laverdefelipe486@gmail.com>
2024-03-17 21:11:20 -0500

    Borrando código muerto y ademas añadiendo el final del juego

--- Code/Juego.py
+++ Code/Juego.py
@@ -1,44 +1,50 @@
 
-
 from Constantes import *
 from Jugador import *
 from Pantalla import *
-#from Tumbas import *
+# from Tumbas import *
 from Zombies import *
 
-import pygame 
+import pygame
 import sys
-import random
 from pygame.sprite import Sprite
 
+
 def main():
-  pygame.init()
-  background_image = pygame.image.load("../imagenes/fondo.jpg").convert()  
-  screen = pygame.display.set_mode(Tamaño_pantalla)
-  juanito = Juan(Tamaño_pantalla)
-  zombies = []
-  x = 0
-  while 1:
+    pygame.init()
+    # Titulo del juego cuando se ejecuta
+    pygame.display.set_caption("Juan y sus Zombies")
+    background_image = pygame.image.load("../imagenes/fondo.jpg").convert()
+    screen = pygame.display.set_mode(Tamaño_pantalla)
+    juanito = Juan(Tamaño_pantalla)
+    zombies = []
+    x = 0
+    final2 = True
+    while 1:
 
-      for event in pygame.event.get():
-  	    if event.type == pygame.QUIT:
-  		    sys.exit()
-      x_relativa = x % background_image.get_rect().width
-      screen.blit(background_image, (x_relativa -
-                    background_image.get_rect().width, 0))
-      if x_relativa < Tamaño_pantalla[0]:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        x_relativa = x % background_image.get_rect().width
+        screen.blit(background_image, (x_relativa -
+                                       background_image.get_rect().width, 0))
+        if x_relativa < Tamaño_pantalla[0]:
             screen.blit(background_image, (x_relativa, 0))
-      x -= 1
-      RELOJ.tick(FPS)
 
-      juanito.animar()
-      screen.blit(juanito.imagen, juanito.rect)
-      juanito.balas.draw(screen)
-      juanito.espadas.draw(screen)
-      si(juanito,zombies)
-   
-      pygame.display.update()
-      pygame.time.delay(10)
+        x -= 1
+
+        RELOJ.tick(FPS)
+
+        if final2 == True:
+            juanito.animar()
+            screen.blit(juanito.imagen, juanito.rect)
+
+        juanito.balas.draw(screen)
+        juanito.espadas.draw(screen)
+        final2 = si(juanito, zombies)
+        pygame.display.update()
+        pygame.time.delay(10)
+
 
 if __name__ == "__main__":
     main()
--- Code/Pantalla.py
+++ Code/Pantalla.py
@@ -19,33 +19,22 @@ pygame.mixer.music.set_volume(0.05)
 def pantalla():
     x = 0
 
-
     background_image = pygame.image.load("../imagenes/fondo.jpg").convert()
 
-    # Titulo del juego cuando se ejecuta
-    pygame.display.set_caption("Juan y sus Zombies")
-
-    # Bucle principal del juego
-    """while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()"""
-
-        # Calcula la posición relativa del fondo para hacer un desplazamiento continuo
+    # Calcula la posición relativa del fondo para hacer un desplazamiento continuo
     x_relativa = x % background_image.get_rect().width
 
-        # Dibuja la imagen de fondo en la pantalla
+    # Dibuja la imagen de fondo en la pantalla
     screen.blit(background_image, (x_relativa -
-                    background_image.get_rect().width, 0))
+                                   background_image.get_rect().width, 0))
     pygame.display.update()
 
-        # Dibuja la imagen de fondo nuevamente para hacer el desplazamiento continuo
+    # Dibuja la imagen de fondo nuevamente para hacer el desplazamiento continuo
     if x_relativa < Tamaño_pantalla[0]:
-            screen.blit(background_image, (x_relativa, 0))
+        screen.blit(background_image, (x_relativa, 0))
 
         # Desplazamiento horizontal del fondo
     x -= 1
 
-        # Controla la velocidad del juego
+    # Controla la velocidad del juego
     RELOJ.tick(FPS)
-
